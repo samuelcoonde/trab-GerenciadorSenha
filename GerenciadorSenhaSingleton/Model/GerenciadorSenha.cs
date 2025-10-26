e4343855c8e633e5b5bb95767acb94a8f8dc0221
samuel <samuelconnde@gmail.com>
2025-10-26 17:52:16 -0300

    Subindo trabalho

---
+++ GerenciadorSenhaSingleton/Model/GerenciadorSenha.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorSenhaSingleton.Model
+{
+    public class GerenciadorSenha
+    {
+        private static GerenciadorSenha _instance;
+        public List<int> listaSenhas = new List<int>();
+        public int proxSenha = 1;
+
+        private GerenciadorSenha() { }
+
+        public static GerenciadorSenha Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new GerenciadorSenha();
+                }
+                return _instance;
+            }
+        }
+
+        public int GetNovaSenha()
+        {
+            listaSenhas.Add(proxSenha);
+            return proxSenha++;
+        }
+
+        public List<int> GetListaSenhas()
+        {
+            return listaSenhas;
+        }
+
+        public void ChamarProximaSenha()
+        {
+            if (listaSenhas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma senha na fila!");
+                return;
+            }
+
+            int senha = listaSenhas[0];
+            listaSenhas.RemoveAt(0);
+            Console.WriteLine($"Chamando senha: {senha}");
+        }
+
+    }
+}
---
+++ GerenciadorSenhaSingleton/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using GerenciadorSenhaSingleton.Model;
+
+namespace GerenciadorSenhaSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorSenha gerenciador = GerenciadorSenha.Instance;
+            string opcao;
+
+            do
+            {
+                Console.WriteLine("\n===== GERENCIADOR DE SENHAS =====");
+                Console.WriteLine("1 - Gerar nova senha");
+                Console.WriteLine("2 - Chamar próxima senha");
+                Console.WriteLine("3 - Mostrar todas as senhas");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        int novaSenha = gerenciador.GetNovaSenha();
+                        Console.WriteLine($"Senha gerada: {novaSenha}");
+                        break;
+
+                    case "2":
+                        gerenciador.ChamarProximaSenha();
+                        break;
+
+                    case "3":
+                        List<int> lista = gerenciador.GetListaSenhas();
+                        if (lista.Count == 0)
+                        {
+                            Console.WriteLine("Nenhuma senha pendente.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Senhas pendentes:");
+                            foreach (int s in lista)
+                            {
+                                Console.WriteLine($"- {s}");
+                            }
+                        }
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Encerrando...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            } while (opcao != "0");
+        }
+    }
+}
